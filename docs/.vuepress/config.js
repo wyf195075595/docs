03828a885bae44a472bc7d7ce05c1fcecf85c010
wyf195075595 <1950755953@qq.com>
2023-04-18 17:58:04 +0800

    新增ussue

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,15 @@
 /*
  * @Description: 
  * @Author:  
+ * @Date: 2023-04-18 08:36:26
+ * @LastEditTime: 2023-04-18 16:28:25
+ * @LastEditors:  
+ */
+/*
+ * @Description: 
+ * @Author:  
  * @Date: 2023-04-17 15:50:18
- * @LastEditTime: 2023-04-18 08:37:55
+ * @LastEditTime: 2023-04-18 15:43:30
  * @LastEditors:  
  */
 module.exports = {
@@ -10,9 +17,18 @@ module.exports = {
     description: "描述",
     base: "/docs/",
     head: [
-        ["link", { rel: "icon", href: "/hero.png"}],
-        ["meta", { name: "keywords", content: "vuepress 介绍"}],
-        ["meta", { name: "author", content: "小趴菜"}],
+      ["meta", { name: "keywords", content: "vuepress 介绍"}],
+      ["meta", { name: "author", content: "小趴菜"}],
+      // PWA 配置如下
+      ["link", { rel: "icon", href: "/hero.png"}],
+      ['link', { rel: 'manifest', href: '/manifest.json' }],
+      ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+      ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+      ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+      ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+      ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+      ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+      ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
     themeConfig: {
       logo: "/hero.png",
@@ -31,6 +47,26 @@ module.exports = {
           }
       ],
       sidebar: "auto"
+    },
+    // 【离线web页面】只有在你能够使用 SSL 部署您的站点时才能启用此功能，因为 service worker 只能在 HTTPs 的 URL 下注册。
+    plugins: {
+      '@vuepress/pwa': {
+        serviceWorker: true,
+        updatePopup: {
+          message: "更新新的内容了！",
+          buttonText: "点我更新"
+        }
+      },
+      '@vssue/vuepress-plugin-vssue': {
+        // 设置 `platform` 而不是 `api`
+        platform: 'github-v4',
+        // 其他的 Vssue 配置
+        owner: 'wyf195075595',
+        repo: 'docs',
+        clientId: 'f9bd7443deec2ef4d24f',
+        clientSecret: '9603fc19fd6e63ef3443ba061dfd204416e46bf9',
+        autoCreateIssue: true
+      },
     }
   }
   
---
+++ docs/.vuepress/theme/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Author:  
+ * @Date: 2023-04-18 16:35:33
+ * @LastEditTime: 2023-04-18 16:39:57
+ * @LastEditors:  
+ */
+module.exports = {
+    extend: '@vuepress/theme-default'
+  }
